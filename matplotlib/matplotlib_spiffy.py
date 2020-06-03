b8c714591bdc57c572db17b964180f4f3a081a6a
bkm <bastian.marquis@protonmail.com>
2020-06-03 17:09:01 +0200

    WIP annotated heatmap

--- matplotlib/matplotlib_spiffy.py
+++ matplotlib/matplotlib_spiffy.py
@@ -8,8 +8,8 @@ def heatmap(ax, data, row_labels, col_labels):
 
     im = ax.imshow(data)
 
-    ax.set_xticks(np.arange(data.shape[0]))
-    ax.set_yticks(np.arange(data.shape[1]))
+    ax.set_xticks(np.arange(data.shape[0]), minor=True)
+    ax.set_yticks(np.arange(data.shape[1]), minor=True)
     ax.set_xticklabels(row_labels)
     ax.set_yticklabels(col_labels)
 
