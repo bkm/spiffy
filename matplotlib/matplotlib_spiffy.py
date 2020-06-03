1ba70a6b2eb013e019f840d50848d234eda0b0e3
bkm <bastian.marquis@protonmail.com>
2020-06-03 17:48:19 +0200

    WIP: annotated heatmap

--- matplotlib/matplotlib_spiffy.py
+++ matplotlib/matplotlib_spiffy.py
@@ -4,18 +4,36 @@ import matplotlib.pyplot as plt
 import matplotlib
 
 
-def heatmap(ax, data, row_labels, col_labels):
+# Creates an annotated heatmap from data
+# 
+# TODO: if matrix is symmetric, allow the choice of whether it should be upper
+# or lower diagonal
+def heatmap(ax, data, row_labels, col_labels, symmetric=False):
 
     im = ax.imshow(data)
 
-    ax.set_xticks(np.arange(data.shape[0]), minor=True)
-    ax.set_yticks(np.arange(data.shape[1]), minor=True)
+    size_x = data.shape[0]
+    size_y = data.shape[1]
+
+    ax.set_xticks(np.arange(size_x))
+    ax.set_yticks(np.arange(size_y))
     ax.set_xticklabels(row_labels)
     ax.set_yticklabels(col_labels)
 
-    ax.grid(which="minor", color="w", linestyle="-", linewidth=3)
+    ax.set_xticks(np.arange(data.shape[0])+1-.5, minor=True)
+    ax.set_yticks(np.arange(data.shape[1])+1-.5, minor=True)
+    ax.tick_params(which="minor", bottom=False, left=False)
 
+    plt.setp(ax.get_xticklabels(), rotation=45, rotation_mode="anchor")
+
+    txt_formatter = matplotlib.ticker.StrMethodFormatter("{x:.2f}")
+
+    ax.grid(which="minor", color="w", linestyle="-", linewidth=3)
     for edge, spine in ax.spines.items():
         spine.set_visible(False)
+
+    for i in range(size_x):
+        for j in range(size_y):
+            im.axes.text(j, i, txt_formatter(data[i, j], None))
 
     return im
