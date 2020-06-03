20dd9c7a0ade6356d04ccc0ff66aee208027b701
bkm <bastian.marquis@protonmail.com>
2020-06-03 23:04:38 +0200

    WIP: heatmap

--- matplotlib/matplotlib_spiffy.py
+++ matplotlib/matplotlib_spiffy.py
@@ -4,14 +4,32 @@ import matplotlib.pyplot as plt
 import matplotlib
 
 
+def annotate_heatmap(im, data, format_string="{x:.2f}", text_params={}, symmetric=False,
+        get_text_color = None):
+    size_x = data.shape[0]
+    size_y = data.shape[1]
+
+    txt_formatter = matplotlib.ticker.StrMethodFormatter(format_string)
+    txt_args = dict(horizontalalignment = "center", verticalalignment = "center")
+    txt_args.update(text_params)
+
+    for i in range(size_x):
+        for j in range(size_y):
+            if symmetric and j>i:
+                txt_args["color"] = "white"
+            elif get_text_color != None:
+                txt_args["color"] = get_text_color(data[i][j])
+            else:
+                txt_args["color"] = "black"
+            im.axes.text(j, i, txt_formatter(data[i, j], None), **txt_args)
+
 # Creates an annotated heatmap from data
 # 
 # TODO: if matrix is symmetric, allow the choice of whether it should be upper
 # or lower diagonal
+# TODO: for now, the data is in RGB format and doesn't require a cmap
 def heatmap(ax, data, row_labels, col_labels, symmetric=False):
-
     im = ax.imshow(data)
-
     size_x = data.shape[0]
     size_y = data.shape[1]
 
@@ -26,15 +44,9 @@ def heatmap(ax, data, row_labels, col_labels, symmetric=False):
 
     plt.setp(ax.get_xticklabels(), rotation=45, rotation_mode="anchor", ha="right")
 
-    txt_formatter = matplotlib.ticker.StrMethodFormatter("{x:.2f}")
-    txt_args = dict(horizontalalignment = "center", verticalalignment = "center")
 
     ax.grid(which="minor", color="w", linestyle="-", linewidth=3)
     for edge, spine in ax.spines.items():
         spine.set_visible(False)
-
-    for i in range(size_x):
-        for j in range(size_y):
-            im.axes.text(j, i, txt_formatter(data[i, j], None), txt_args)
 
     return im
